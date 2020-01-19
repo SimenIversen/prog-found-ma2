b32cfff63befdbceac1b37696fcb1eb1a7282503
SimenIversen <44866532+SimenIversen@users.noreply.github.com>
2020-01-19 19:37:42 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 // question 1
 /* Create an array called petArray with 3 string items in it: "cat", "cow", "dog". */
-var petArray = ["cat", "cow", "dog"];
+//var petArray = ["cat", "cow", "dog"];
 
 // question 2
 /* Console log the value of the second item in petArray. */
-console.log(petArray [1]);
+//console.log(petArray [1]);
 
 // question 3
 /* Add the item "sheep" to petArray using the push method. */
-petArray.push("sheep");
+//etArray.push("sheep");
 
 // question 4
 /* Console log how many items there are now in petArray using the length method. */
-console.log(petArray.length);
+//console.log(petArray.length);
 
 // question 5
 /* Create an object called catObject with 3 properties: name, colour, age. Add appropriate values (and type of values) for each of these properties. */
-var catObject = {
-    name: "boddy",
-    colour: "black",
-    age: 6
-};
+//var catObject = {
+  //  name: "boddy",
+    //colour: "black",
+   // age: 6
+//};
 
 // question 6
 /* Console log the value of the third property of catObject. */
-console.log(catObject[2]);
+//console.log(catObject[2]);
 
 // question 7
 /* Take the value of catObject (the {} part) and place it inside an array called catArray. */
+//var catArray = [{
+    //name: "boddy",
+    //colour: "black",
+    //age: 6
+}
+]
 
 
 // question 8
-/* Loop throughcatArray and console log each property of each object inside (there will be only 1 object inside). */
-
+/* Loop through catArray and console log each property of each object inside (there will be only 1 object inside). */
+//for(var i = 0; i < catArray; i++){
+   // console.log(catArray(i))
+}
 
 // question 9
 /* Create a function called logToConsole. Th function should accept 1 argument. Decide on the name of the argument and console log its value inside the function. */
-
+//function logToConsole(){
+    //console.log("dog");
+}
 
 // question 10
 /* Call the logToConsole function and pass in the value "donkey". */
+//logToConsole();
 
+//function logToConsole(){
+//console.log("dunkey");
+}
+//logToConsole();
